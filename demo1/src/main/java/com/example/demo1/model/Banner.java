b8c1027578911c0a1feaa8aaa824c7da0b820ac1
23130194_Nguyễn Võ Quang Minh <23130194@st.hcmuaf.edu.vn>
2025-12-28 23:10:23 +0700

    Thêm file banner.java
    Thêm file banner để tạo quảng cáo trong các trang

---
+++ demo1/src/main/java/com/example/demo1/model/Banner.java
@@ -0,0 +1,70 @@
+package com.example.demo1.model;
+
+public class Banner {
+    private int id;
+    private String name;
+    private String image;
+    private String position;
+    private int display_order;
+    private String start_time;
+    private String end_time;
+
+    public Banner() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getDisplay_order() {
+        return display_order;
+    }
+
+    public void setDisplay_order(int display_order) {
+        this.display_order = display_order;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+}
