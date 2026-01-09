48526c555985fe4f55434524a8f77e9a3c3dd306
PhanThaoTien <23130328@st.hcmuaf.edu.vn>
2026-01-09 21:46:38 +0700

    add OrderDetail.java

---
+++ demo1/src/main/java/com/example/demo1/model/OrderDetail.java
@@ -0,0 +1,43 @@
+package com.example.demo1.model;
+
+import java.util.List;
+
+public class OrderDetail {
+    private Order order;
+    private User customer; // Giả sử bạn có một lớp User
+    private List<OrderItem> items;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(Order order, User customer, List<OrderItem> items) {
+        this.order = order;
+        this.customer = customer;
+        this.items = items;
+    }
+
+    // Getters and Setters
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+}
